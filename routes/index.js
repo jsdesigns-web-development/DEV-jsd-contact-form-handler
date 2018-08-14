ba4939ba6a16579e8b1a06497804a6db84b5e6cf
Brian Jason <brianjason@gmail.com>
2018-08-14 12:34:00 -0700

    app now successfully sends to recipient found in db

--- routes/index.js
+++ routes/index.js
@@ -4,40 +4,55 @@ const express = require('express');
 const bodyParser = require("body-parser");
 
 const router = express.Router();
+const Database = require("../lib/db.js");
 const Mailer = require("../lib/");
 //const log = require('../lib/log.js');
 
+var db = new Database.contactsDB();
+var mailer = new Mailer.init(db);
 
 router.use(function timeLog(req, res, next) {
   next();
 });
 
 router.post('/contact', function(req,res, next) {
-  console.log("message received")
-  console.log(req.body.domain);
-  var mailer = new Mailer.init(req.body);
+  console.log("message submitted")
+  mailer.formData = req.body;
+
+  db.Contacts.find(function(err, domain){
+    console.log(mailer.formData.domain);
+    //db.Contacts -- START
+    let recipientEmailAddress = domain[0].address[0];
+
+    // COMPOSE MESSAGE STRING
+    if(req.body.message.trim() != "" || !req.body.message) {
+      mailer.mailOptions.text = mailer.composeMessage(req.body, "text");
+      mailer.mailOptions.html = mailer.composeMessage(req.body, "html");
+    }
+
+    // EMAIL FORM DATA TO RECIPIENT
+    var result = mailer.send(recipientEmailAddress);
+    /*
+    */
+
+    res.send((result) ? ('Message sent to ' + recipientEmailAddress) : 'Message not sent.');
+    //db.Contacts -- END
+  });
 
   /*
   // VALIDATE EMAIL
   let validEmail = mailer.validate(req.body);
   if (!validEmail[0]) { console.error(validEmail[1]) }
   */
-  // COMPOSE MESSAGE STRING
-  //var messageTXT = mailer.composeMessage(req.body, "text");
-  //var messageHTML = mailer.composeMessage(req.body, "html");
-  mailer.mailOptions.text = mailer.composeMessage(req.body, "text");
-  mailer.mailOptions.html = mailer.composeMessage(req.body, "html");
 
-  // LOOKUP RECIPIENT
-  var recipient = mailer.lookupRecipient(req.body.domain);
+
+  /*
   if (!recipient) {
     console.error("ERROR: " + req.body.domain + " domain does not exist")
   }
-
-  // EMAIL FORM DATA TO RECIPIENT
-  var result = mailer.send();
-  /*
   */
+
+
 
   // LOG RESULTS
   /*
@@ -49,7 +64,7 @@ router.post('/contact', function(req,res, next) {
   /*
   console.log(result);
   */
-  res.send((result) ? 'message sent' : 'message failed. message not sent.');
+  //res.send((result) ? 'message sent' : 'message failed. message not sent.');
 });
 
 module.exports = router;
